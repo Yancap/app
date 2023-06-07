37bbf9e09936fa0f93683b4e61e51a533b99be37
Yancap <yangabriel3001f@gmail.com>
2023-06-07 17:42:45 -0300

    first commit

---
+++ src/components/Aside/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const index = () => {
+  return (
+    <>
+        <header>
+            <h1>Yan Gabriel Ferreira</h1>
+            <h3>Desenvolvedor Web</h3>
+        </header>
+        <nav>
+
+        </nav>
+        <footer>
+
+        </footer>
+    </>
+  )     
+}
