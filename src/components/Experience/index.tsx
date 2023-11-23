a3f147e97f3666b0ff7ccbb2ee3a31a2ea14efe9
Yancap <yangabreil3001f@gmail.com>
2023-11-23 19:28:13 -0300

    add new job

--- src/components/Experience/index.tsx
+++ src/components/Experience/index.tsx
@@ -7,13 +7,39 @@ export const Experience = () => {
         <h2 className='text-purple-200 font-bold text-2xl border-b-2 border-purple-800 pb-2 md:pb-0 md:border-0'>
             Minhas Experiencias
         </h2>
-        
         <div className="flex flex-wrap justify-between flex-col md:flex-row gap-4 rounded-lg cursor-pointer p-4 hover:bg-purple-800/20" 
             onClick={() => { 
                 window.location.href = "https://www.ibm.com/br-pt"
             }}>
             <h5 className='text-purple-500 text-[14px] pt-1 md:w-3/12 min-w-fit'>
-                Set 2023 - Atualmente
+                Nov 2023 - Atualmente
+            </h5>
+            <div className='md:w-8/12'>
+                <h3 className='text-white text-lg'>
+                    Aplication Developer - <strong>IBM</strong>
+                </h3>
+                <h4 className='text-purple-500 mb-1'>
+                    Estágiario Técnico
+                </h4>
+                <p className='text-purple-100 text-[13px]'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam maximus urna eleifend mauris lobortis, 
+                    non pulvinar magna finibus. Integer ut vestibulum leo. Vestibulum ante ipsum primis in faucibus orci luctus
+                    et ultrices posuere cubilia curae; Nunc nec est non eros elementum rhoncus dignissim sit amet nunc. 
+                    Fusce vulputate.
+                </p>
+                <div className='flex flex-wrap gap-2 py-4'>
+                    <Tech text='Angular' tech='angular'/>
+                    <Tech text='Typescript' tech='typescript'/>
+                    <Tech text='Java' tech='java'/>
+                </div>
+            </div>
+        </div>
+        <div className="flex flex-wrap justify-between flex-col md:flex-row gap-4 rounded-lg cursor-pointer p-4 hover:bg-purple-800/20" 
+            onClick={() => { 
+                window.location.href = "https://www.ibm.com/br-pt"
+            }}>
+            <h5 className='text-purple-500 text-[14px] pt-1 md:w-3/12 min-w-fit'>
+                Set 2023 - Nov 2023
             </h5>
             <div className='md:w-8/12'>
                 <h3 className='text-white text-lg'>
